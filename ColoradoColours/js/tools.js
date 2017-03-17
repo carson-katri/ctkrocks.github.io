d2b197b1c9c2816846a508fde97d02aad263ee3c
Carson <Carson.katri@gmail.com>
2017-03-17 11:35:15 -0400

    Fixed the URL creator

--- ColoradoColours/js/tools.js
+++ ColoradoColours/js/tools.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 });
 
 function copy() {
-	var url = "file:///Users/Ryan/Downloads/ColoradoColours/index.html?name=" + $("#name").val() + "&invoice=" + $("#invoice").val() +  "&amount=" + $("#amount").val() + "#pay";
+	var url = "https://ctkrocks.github.io/ColoradoColours/index.html?name=" + $("#name").val() + "&invoice=" + $("#invoice").val() +  "&amount=" + $("#amount").val() + "#pay";
 	url = url.replace(" ", "%20");
 	$("#url").val(url);
 };
